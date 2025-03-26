0e100d6d82a3fce8145743b3ca6d6979ebbfacb1
AngelRamiz <amoralesr16@miumg.edu.gt>
2025-03-26 12:09:17 -0600

    Implementación de lista doblemente enlazada con métodos adicionales

--- src/main/java/umg/edu/progra/listas/Principal.java
+++ src/main/java/umg/edu/progra/listas/Principal.java
@@ -1,5 +1,7 @@
 package umg.edu.progra.listas;
 
+import umg.edu.progra.listas.doblementeEnlazada.DoblementeEnlazada;
+
 public class Principal {
 
     public static void main(String[] args) {
@@ -56,5 +58,57 @@ public class Principal {
 
         // Mostrar el tamaño de la lista
         System.out.println("\nTamano de la lista: " + lista.obtenerTamano());
+        
+        //TAREA LISTA DOBLEMENTE ENLAZADA
+    
+        System.out.println("\n\n--- LISTA DOBLEMENTE ENLAZADA ---");
+        DoblementeEnlazada listaDoble = new DoblementeEnlazada();
+
+        // Insertar elementos al final
+        listaDoble.insertAtEnd(10);
+        listaDoble.insertAtEnd(20);
+        listaDoble.insertAtEnd(30);
+        listaDoble.insertAtEnd(40);
+
+        System.out.println("\nLista despues de insertar al final:");
+        listaDoble.displayForward();
+
+        // Insertar elementos al inicio
+        listaDoble.insertAtBeginning(5);
+        listaDoble.insertAtBeginning(1);
+
+        System.out.println("\nLista despues de insertar al inicio:");
+        listaDoble.displayForward();
+
+        // Contar nodos
+        System.out.println("\nNumero de nodos en la lista: " + listaDoble.count());
+
+        // Insertar despues de un valor especifico
+        listaDoble.insertAfter(20, 25);
+        System.out.println("\nLista despues de insertar 25 despues de 20:");
+        listaDoble.displayForward();
+
+        // Buscar un valor en la lista
+        System.out.println("\n¿Existe el valor 25 en la lista? " + listaDoble.search(25));
+        System.out.println("¿Existe el valor 50 en la lista? " + listaDoble.search(50));
+
+        // Eliminar un valor
+        listaDoble.delete(10);
+        System.out.println("\nLista despues de eliminar 10:");
+        listaDoble.displayForward();
+
+        // Mostrar la lista en orden inverso
+        System.out.println("\nLista en orden inverso:");
+        listaDoble.displayBackward();
+
+        // Invertir la lista
+        listaDoble.reverse();
+        System.out.println("\nLista despues de invertir:");
+        listaDoble.displayForward();
+
+        // Contar nodos despues de todas las operaciones
+        System.out.println("\nNumero de nodos en la lista despues de todas las operaciones: " + listaDoble.count());
     }
-}
+
+    }
+	
